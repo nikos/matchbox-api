1aa9ee3ffffca7bac410f00838e91f8d6a6d363b
Niko Schmuck <n.schmuck@nava.de>
2014-12-13 21:57:48 +0100

    add timestamp

--- src/matchbox/models/rating.clj
+++ src/matchbox/models/rating.clj
@@ -1,5 +1,6 @@
 (ns matchbox.models.rating
-  (:import (org.bson.types ObjectId))
+  (:import (org.bson.types ObjectId)
+           (java.util Date))
   (:require [monger.collection :as coll]
             [matchbox.config :refer [db coll-ratings]]))
 
@@ -10,7 +11,9 @@
   (coll/find-one-as-map db coll-ratings {:_id (ObjectId. id)}))
 
 (defn create [rating]
-  (coll/insert-and-return db coll-ratings rating))
+  (let [now (Date.)
+        timestamp (.getTime now)]
+    (coll/insert-and-return db coll-ratings (assoc rating :created_at timestamp))))
 
 (defn update [id rating]
   (coll/update-by-id db coll-ratings (ObjectId. id) {:name (rating :name)} {:multi false}))
